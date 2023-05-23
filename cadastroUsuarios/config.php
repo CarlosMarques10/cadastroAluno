40c9f8f1de7c3fdba5091b3602f8577ad6bab49c
Carlos Marques <99378021+CarlosMarques10@users.noreply.github.com>
2023-05-23 07:31:34 -0300

    Delete cadastroUsuarios directory

--- cadastroUsuarios/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_name = 'cadastroUsuarios';
-$db_host = 'localhost';
-$db_user = 'postgres';
-$db_pass = '12345678';
-
-$pdo = new PDO("pgsql:dbname=".$db_name.";host=".$db_host,$db_user,$db_pass);
